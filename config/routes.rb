240a19e357221d88f2bd07d1778f8091bc92c267
Danny Collier <dcollie2@gmu.edu>
2017-09-13 19:04:37 +0500

    Route rote  root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :sections
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+  root to: 'sections#index'
+
 end
