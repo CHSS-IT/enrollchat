9fafd0f7f00d797e06d676d6641bb3357e1e1ca8
Danny Collier <dcollie2@gmu.edu>
2017-09-14 10:03:58 +0500

    Remove unnecessary actions from sections.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :sections
+  resources :sections, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   root to: 'sections#index'
 
