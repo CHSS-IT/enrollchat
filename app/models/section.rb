e3823dcb150d2f07e7693981e6f96efb2f8b1ab2
Danny Collier <dcollie2@gmu.edu>
2017-09-15 18:09:01 +0500

    Additional formatting tweaks.

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,6 +3,6 @@ class Section < ApplicationRecord
   has_many :comments
 
   def section_and_number
-    "#{course_description}: #{section_id}"
+    "#{course_description}-#{section_number}"
   end
 end
