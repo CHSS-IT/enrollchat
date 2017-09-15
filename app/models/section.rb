42b5810e7250fa442fe5cbb78bead1f923490d4b
Danny Collier <dcollie2@gmu.edu>
2017-09-15 18:15:28 +0500

    Lead section numbers with zeros.

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,11 @@ class Section < ApplicationRecord
 
   has_many :comments
 
+  def section_number_zeroed
+    section_number.to_s.rjust(3, "0")
+  end
+
   def section_and_number
-    "#{course_description}-#{section_number}"
+    "#{course_description}-#{section_number_zeroed}"
   end
 end
