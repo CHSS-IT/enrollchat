c25ddca237f53c37eafd10ac94a409cdd8e31de9
Craig Zaccaro <czaccaro@live.com>
2017-09-20 19:16:56 -0400

    Remove unused devise options. Add test helpers for cas login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,12 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+end
+
+class ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+  Warden.test_mode!
+end
