2ce3940d9bdb87df339f1d876dc55ac25caa5226
Danny Collier <dcollie2@gmu.edu>
2017-09-21 11:44:47 +0500

    Finish removing ancestry. Fix comments column.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -85,6 +85,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:user_id, :section_id, :body, :ancestry, :ancestry_depth, :section_id)
+      params.require(:comment).permit(:user_id, :section_id, :body, :section_id)
     end
 end
--- app/views/comments/_comment.json.jbuilder
+++ app/views/comments/_comment.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! comment, :id, :user_id, :section_id, :body, :ancestry, :ancestry_depth, :created_at, :updated_at
+json.extract! comment, :id, :user_id, :section_id, :body, :created_at, :updated_at
 json.url comment_url(comment, format: :json)
