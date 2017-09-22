4015b70492b15cebf6809d1b4c86d6bbaed463c7
Craig Zaccaro <czaccaro@live.com>
2017-09-22 13:52:46 -0400

    Get tests working behind CAS login. Comment out section tests that we don't have controller actions for yet.

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,47 +2,48 @@ require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @section = sections(:one)
     @comment = comments(:one)
+    login_as users(:one)
   end
 
   test "should get index" do
-    get comments_url
+    get section_comments_url(@section)
     assert_response :success
   end
 
   test "should get new" do
-    get new_comment_url
+    get new_section_comment_url(@section)
     assert_response :success
   end
 
   test "should create comment" do
     assert_difference('Comment.count') do
-      post comments_url, params: { comment: { body: @comment.body, section_id: @comment.section_id, user_id: @comment.user_id } }
+      post section_comments_url(@section), params: { comment: { body: @comment.body, section_id: @comment.section_id, user_id: @comment.user_id } }
     end
-
-    assert_redirected_to comment_url(Comment.last)
+    assert_redirected_to sections_url
   end
 
   test "should show comment" do
-    get comment_url(@comment)
+    get section_comment_url(@section, @comment)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_comment_url(@comment)
+    get edit_section_comment_url(@section, @comment)
     assert_response :success
   end
 
   test "should update comment" do
-    patch comment_url(@comment), params: { comment: { body: @comment.body, section_id: @comment.section_id, user_id: @comment.user_id } }
-    assert_redirected_to comment_url(@comment)
+    patch section_comment_url(@section, @comment), params: { comment: { body: @comment.body, section_id: @comment.section_id, user_id: @comment.user_id } }
+    assert_redirected_to section_comment_url(@section, @comment)
   end
 
   test "should destroy comment" do
     assert_difference('Comment.count', -1) do
-      delete comment_url(@comment)
+      delete section_comment_url(@section, @comment)
     end
 
-    assert_redirected_to comments_url
+    assert_redirected_to section_comments_url
   end
 end
