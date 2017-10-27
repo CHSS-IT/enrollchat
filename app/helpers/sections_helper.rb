c36f54b31f6406f82d8045325461dea7245e1848
Danny Collier <dcollie2@gmu.edu>
2017-10-27 09:39:31 +0500

    Overarchitect the status labels.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -13,5 +13,19 @@ module SectionsHelper
   end
 
 
+  def flagged_icon(section)
+    if section.flagged_as.present?
+      icon_class =     case section.flagged_as
+                         when "canceled"
+                           'fa fa-ban fa-pull-left'
+                         when "waitlisted"
+                           'fa fa-list-ol fa-pull-left'
+                         when "under-enrolled"
+                           'fa fa-level-down fa-pull-left'
+                       end
+      text = tag.i({class: icon_class, title: section.flagged_as.capitalize, alt: section.flagged_as.capitalize}) + section.flagged_as.capitalize
+    end
+    return text.html_safe if text.present?
+  end
 
 end
