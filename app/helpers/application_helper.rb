63f821be8affff3f86d9c06069beb1dd011888bc
Danny Collier <dcollie2@gmu.edu>
2017-10-29 18:01:12 +0500

    Add most recent five comments to update box.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def basic_date(date)
     date.strftime('%B %-d, %Y')
   end
+
+  def basic_datetime(date)
+    date.strftime('%B %-d at %l:%M %P')
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :section, required: true
+
+  scope :recent, -> { order(created_at: :desc).limit(5) }
 end
