95b946f6ae734666b09b70ee25d4aaafb4792a11
Craig Zaccaro <czaccaro@live.com>
2017-11-06 09:31:56 -0500

    Fix typo and strengthen sign-in tests.

--- test/integration/sign_in_test.rb
+++ test/integration/sign_in_test.rb
@@ -17,14 +17,14 @@ class SignInTest < ActionDispatch::IntegrationTest
     login_as users(:two)
     get sections_url
     assert_response :success
-    assert_select "a", "EnrollChat"
+    assert_select "a", "Filter"
   end
 
-  test "should send users who are already signedin to main sections page as the authenticated root" do
+  test "should send users who are already signed in to main sections page as the authenticated root" do
     login_as users(:two)
     get root_url
     assert_response :success
-    assert_select "a", "EnrollChat"
+    assert_select "a", "Filter"
   end
 
   test "should send unregistered users to unregistered page" do
