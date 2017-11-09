e7b1b838327e0ac1854f180a3d5cb375cf678cea
Danny Collier <dcollie2@gmu.edu>
2017-11-09 20:53:08 +0500

    Ignore comments on deleted sections in activity box.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :section, required: true
 
-  scope :recent, -> { order(created_at: :desc).limit(5) }
+  scope :recent, -> { joins(:section).where("sections.delete_at is null").order(created_at: :desc).limit(5) }
 
 end
