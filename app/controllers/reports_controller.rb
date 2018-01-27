53c33b33face869b1b7fe4a5fcf4bba950fc3aae
Danny Collier <dcollie2@gmu.edu>
2018-01-27 21:24:00 +0500

    Constrain report to selected term.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,13 @@
 class ReportsController < ApplicationController
   def index
     @sections = Section.in_term(@term).order(:department)
-    @max_date = @sections.maximum(:updated_at)
-    @not_canceled = @sections.not_canceled.size
-    @canceled = @sections.canceled.size
-    @full = @sections.full.size
-    @under_enrolled = @sections.under_enrolled.size
-    @over_enrolled = @sections.over_enrolled.size
-    @sections = @sections.group_by { |s| s.department}
+    @max_date = @sections.in_term(@term).maximum(:updated_at)
+    @not_canceled = @sections.in_term(@term).not_canceled.size
+    @canceled = @sections.in_term(@term).canceled.size
+    @full = @sections.in_term(@term).full.size
+    @under_enrolled = @sections.in_term(@term).under_enrolled.size
+    @over_enrolled = @sections.in_term(@term).over_enrolled.size
+    @sections = @sections.in_term(@term).group_by { |s| s.department}
 
 
 
