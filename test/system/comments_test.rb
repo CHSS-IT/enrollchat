815484e6e018ab3a82e67df5cb883639bc660755
Craig Zaccaro <czaccaro@live.com>
2018-01-30 15:29:57 -0500

    Add some additonal test coverage.

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -8,6 +8,7 @@ class CommentsTest < ApplicationSystemTestCase
     assert_text('Criminal Justice-001: Anthony DiNozzo less than a minute ago')
     assert_text('Experiential Learning-001: Jethro Gibbs less than a minute ago')
     refute_text('MyString-001: Jethro Gibbs less than a minute ago')
+    refute_text('MyString-001: Jethro Gibbs 2 days ago')
   end
 
   test 'comment activity feed displays the five most recent notifications for a user when there are no unread notifications and the user has no departments selected' do
