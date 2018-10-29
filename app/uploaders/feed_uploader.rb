6873f56c278216307bb009671ea06add28f430ee
Danny Collier <dcollie2@gmu.edu>
2018-10-29 09:30:45 -0400

    Add reporting during section upload. Clean up workflow.

--- app/uploaders/feed_uploader.rb
+++ app/uploaders/feed_uploader.rb
@@ -1,10 +1,14 @@
 class FeedUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+
+  after :store, :perform_import
+
+  def perform_import(file)
+    ActionCable.server.broadcast 'room_channel',
+                                 message: "<a href='/sections' class='dropdown-item'>Registration data import in process.</a>"
+    ImportWorker.perform_async(self.url.to_s)
+  end
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -13,28 +17,9 @@ class FeedUploader < CarrierWave::Uploader::Base
     nil
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # Allow only Excel files:
+  # Allow only Excel and CSV files:
   def extension_whitelist
     %w(xlsx csv)
   end
