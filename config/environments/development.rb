4c94449cece20923d3f7fb03a2182e48e3bc5107
Craig Zaccaro <czaccaro@live.com>
2019-03-25 11:35:32 -0400

    Move handling of local yaml file back to application config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,11 +73,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_cable.url = "ws://localhost:3000/cable"
-
-  config.before_configuration do
-    env_file = File.join(Rails.root, 'config', 'local_env.yml')
-    YAML.load(File.open(env_file)).each do |key, value|
-      ENV[key.to_s] = value
-    end if File.exist?(env_file)
-  end
 end
