0ae901fc55aee01f77d023382c4d7324cf948187
Craig Zaccaro <czaccaro@live.com>
2020-01-06 16:25:52 -0500

    Fix merge conflict in application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,14 +3,6 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400,1050]
 
-<<<<<<< HEAD
-  include Warden::Test::Helpers
-  Warden.test_mode!
-
-  teardown do
-    Warden.test_reset!
-  end
-=======
   def login_as(user)
     visit test_login_path
     fill_in 'username', with: user.username
@@ -25,8 +17,6 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   def logout
     visit logout_path
   end
-
->>>>>>> master
 end
 
 module BootstrapSelectHelper
