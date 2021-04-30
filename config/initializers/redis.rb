f39991cbbca3c08993aff83bf8c103f89eaf5415
Craig Zaccaro <czaccaro@live.com>
2021-04-30 15:33:43 -0400

    Add sidekiq initializer to handle redis config. Add notes to code for future reference on these redis changes.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
+# Added during upgrade of redis adaptor
+
 $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+# Added during upgrade of redis adaptor
+# https://github.com/mperham/sidekiq/issues/4786
+
+Sidekiq.configure_server do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
