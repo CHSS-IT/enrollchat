556060f1d03d460b23b0694ccb4f8ab876b9dce3
Craig Zaccaro <czaccaro@live.com>
2022-05-14 15:27:25 -0400

    Add channels to asset manifest.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link channels/consumer.js
+//= link channels/index.js
+//= link channels/room_channel.js
+//= link channels/department_channel.js
+
