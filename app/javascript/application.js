55acf7ac0d245f0cd4d9553228357aeea33d5668
Craig Zaccaro <czaccaro@live.com>
2022-10-07 16:34:51 -0400

    turbo and stimulus setup.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@rails/actioncable"
 import "channels"
+import "@hotwired/turbo-rails"
+import "controllers"
