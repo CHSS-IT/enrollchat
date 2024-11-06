8f10b4841a1a4355871e8a85eacc9c38b2f5015a
Craig Zaccaro <czaccaro@live.com>
2024-11-05 22:46:15 -0500

    Update specs.

--- test/integration/scheduler_reporting_emails_test.rb
+++ test/integration/scheduler_reporting_emails_test.rb
@@ -6,31 +6,33 @@ class SchedulerReportingEmailsTest < ActionDispatch::IntegrationTest
   setup do
     Rake::Task.clear
     Enrollchat::Application.load_tasks
-    Time.zone = "Eastern Time (US & Canada)"
   end
 
   teardown do
     Rake::Task.clear
-    Time.zone = "UTC"
   end
 
   # schedule old term purge rake task
   test "schedule old term purge email is generated" do
-    travel_to Time.zone.local(2022, 1, 10, 10, 4, 44) do
-      assert_emails 1 do
-        Rake::Task['scheduler:schedule_old_term_purge'].invoke
+    Time.use_zone("Eastern Time (US & Canada)") do
+      travel_to Time.zone.local(2022, 1, 10, 10, 4, 44) do
+        assert_emails 1 do
+          Rake::Task['scheduler:schedule_old_term_purge'].invoke
+        end
       end
     end
   end
 
   test "schedule old term purge email content" do
-    travel_to Time.zone.local(2022, 1, 10, 10, 4, 44) do
-      Rake::Task['scheduler:schedule_old_term_purge'].invoke
+    Time.use_zone("Eastern Time (US & Canada)") do
+      travel_to Time.zone.local(2022, 1, 10, 10, 4, 44) do
+        Rake::Task['scheduler:schedule_old_term_purge'].invoke
+      end
+      email = ActionMailer::Base.deliveries.last
+      assert_equal ["no-reply@example.com"], email.from
+      assert_equal ["recipient@example.com"], email.to
+      assert_equal 'Terms Marked for Deletion (Triggered in test)', email.subject
+      assert email.body.to_s.include?("<h1>Terms marked for deletion</h1><p>201810</p><p>All sections from these terms will be removed from the system in 30 days.</p>")
     end
-    email = ActionMailer::Base.deliveries.last
-    assert_equal ["no-reply@example.com"], email.from
-    assert_equal ["recipient@example.com"], email.to
-    assert_equal 'Terms Marked for Deletion (Triggered in test)', email.subject
-    assert email.body.to_s.include?("<h1>Terms marked for deletion</h1><p>201810</p><p>All sections from these terms will be removed from the system in 30 days.</p>")
   end
 end
