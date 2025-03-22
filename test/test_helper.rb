04fd32f8303cc1b1374675fa8cb678c415c81555
Craig Zaccaro <czaccaro@live.com>
2025-03-22 14:20:31 -0400

    Attempt to fix ci.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,17 +11,13 @@ require 'minitest/autorun'
 Capybara.server = :puma, { Silent: true }
 
 class ActiveSupport::TestCase
-  parallelize_setup do |worker|
+  setup do
     Setting.create!(current_term: 201810, singleton_guard: 0, undergraduate_enrollment_threshold: 12, graduate_enrollment_threshold: 10, email_delivery: 'scheduled')
-    SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
   end
 
-  parallelize_teardown do
+  teardown do
     Setting.first.destroy
-    SimpleCov.result
   end
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
