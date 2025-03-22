b700389b8551f775588931b8593f8d53a5dee5ee
Craig Zaccaro <czaccaro@live.com>
2025-03-22 14:08:26 -0400

    Attempt to fix ci.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,10 +12,14 @@ Capybara.server = :puma, { Silent: true }
 
 class ActiveSupport::TestCase
   parallelize_setup do |worker|
+    @setting = settings(:one)
+    @section = sections(:one) unless Section.first
     SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
   end
 
   parallelize_teardown do
+    Setting.first.destroy
+    Section.first&.destroy
     SimpleCov.result
   end
   # Run tests in parallel with specified workers
@@ -27,6 +31,16 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
+  setup do
+    @setting = settings(:one)
+    @section = sections(:one) unless Section.first
+  end
+
+  teardown do
+    Setting.first.destroy
+    Section.first&.destroy
+  end
+
   def login_as(user)
     post login_path, params: { username: user.username, password: 'any password' }
   end
