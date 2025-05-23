28c5114c830e812a452ebcd4e26f9797a4c17a06
Craig Zaccaro <czaccaro@live.com>
2025-05-23 13:57:59 -0400

    Only run tests in parallel in CI.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,19 +11,22 @@ require 'minitest/autorun'
 Capybara.server = :puma, { Silent: true }
 
 class ActiveSupport::TestCase
-  # parallelize_setup do |worker|
-  #   Setting.create!(current_term: 201810, singleton_guard: 0, undergraduate_enrollment_threshold: 12, graduate_enrollment_threshold: 10, email_delivery: 'scheduled')
-  #   SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
-  # end
-  #
-  # parallelize_teardown do
-  #   Setting.first.destroy
-  #   SimpleCov.result
-  # end
-  # # Run tests in parallel with specified workers
-  # parallelize(workers: :number_of_processors)
-  #
-  # parallelize threshold: 0 if ENV["CI"].present?
+  if ENV["CI"].present?
+    parallelize_setup do |worker|
+      Setting.create!(current_term: 201810, singleton_guard: 0, undergraduate_enrollment_threshold: 12, graduate_enrollment_threshold: 10, email_delivery: 'scheduled')
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+    end
+
+    parallelize_teardown do
+      Setting.first.destroy
+      SimpleCov.result
+    end
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
+
+    parallelize threshold: 0
+  end
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 end
