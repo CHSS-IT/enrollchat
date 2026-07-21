56db9b6f29090f3909aa26fd5ab358be03aa0271
Craig Zaccaro <czaccaro@live.com>
2026-07-21 11:23:00 -0400

    Switch strong params to expect syntax to satisfy rubocop where possible.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -96,6 +96,6 @@ class CommentsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the permit list through.
   def comment_params
-    params.require(:comment).permit(:user_id, :section_id, :body, :section_id)
+    params.expect(comment: [:user_id, :section_id, :body, :section_id])
   end
 end
